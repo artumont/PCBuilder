96958b8109e2d8c0c4d8187e222067a9a15e8f40
artumont <197291181+artumont@users.noreply.github.com>
2025-02-15 15:50:19 -0600

    Enhance token management in Account component; add token refresh logic and cleanup function

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { isTokenExpired } from '@/utils/auth'
+import { isTokenExpired, attemptRefresh } from '@/utils/auth'
 
 export default function Account() {
 
@@ -18,21 +18,48 @@ export default function Account() {
         }
     }
 
-    const authToken = getToken('authToken')
-    const refreshToken = getToken('refreshToken')
-
-    if (!authToken || isTokenExpired(authToken)) {
-        if (refreshToken && !isTokenExpired(refreshToken)) {
-            // @todo: refresh token
-        }
-        else {
-            window.location.href = '/login'
+    const removeToken = (tokenType: string) => {
+        switch (tokenType) {
+            case 'authToken':
+                document.cookie = 'authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+                break
+            case 'refreshToken':
+                document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+                break
+            default:
+                break
         }
     }
 
+    const handleTokenRefresh = async () => {
+        let authToken = getToken('authToken')
+        const refreshToken = getToken('refreshToken')
+
+        if (!authToken || isTokenExpired(authToken)) {
+            if (refreshToken && !isTokenExpired(refreshToken)) {
+                const token = await attemptRefresh(refreshToken)
+                if (token) {
+                    authToken = token
+                } else {
+                    removeToken('authToken')
+                    removeToken('refreshToken')
+                    window.location.href = '/login'
+                }
+            } else {
+                removeToken('authToken')
+                removeToken('refreshToken')
+                window.location.href = '/login'
+            }
+        }
+        return authToken
+    }
+
+    // handleTokenRefresh()
+    //! Uncomment the above line to run the function on page load
+
     return (
         <main className="flex flex-col items-center w-screen h-screen bg-light dark:bg-dark">
-            
+
         </main>
     );
 }
